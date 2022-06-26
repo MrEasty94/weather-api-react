2508d7b4e83f5bd3578a36ab10e717617b16ab4f
Michael East <michael@michaeleast.dev>
2022-06-26 20:56:01 +0100

    FETCH updated and fully functioning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+import ListItem from './components/ListItem';
+
+
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=6e55d48adff6a34af29e6922dd82682b';
+
 
 function App() {
+  
+  const [weather, setWeather] = useState([]);
+  
+  useEffect(() => {
+    const fetchWeather = async () => {
+      
+      try{
+        const response = await fetch(API_URL);
+        if(!response.ok) throw response;
+        
+        const data = await response.json();
+        
+        setWeather(data);
+        console.log(data);
+      }
+      catch(err) {
+        console.log(err)
+      }
+
+    };
+    fetchWeather();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+      <ListItem data={weather}/>
+  </>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,9 @@
+function ListItem({ data }){
+	return (
+		<>
+			<p>{data.name}</p>
+		</>
+	)
+}
+
+export default ListItem
